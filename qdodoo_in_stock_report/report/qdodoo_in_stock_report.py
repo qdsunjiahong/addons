9a5960258e8c5f8a4dfd5f3819c8fd4494cfdcac
tfs <979581151@qq.com>
2015-09-08 10:29:32 +0800

    修改入库明细表

--- qdodoo_in_stock_report/report/qdodoo_in_stock_report.py
+++ qdodoo_in_stock_report/report/qdodoo_in_stock_report.py
@@ -26,7 +26,7 @@ class qdodoo_in_stock_report(osv.Model):
         'location_id': fields.many2one('stock.location',u'库位', readonly=True),
         'date': fields.datetime(u'日期', readonly=True),
         'partner_id': fields.many2one('res.partner',u'业务伙伴', readonly=True),
-        'default_code': fields.char(u'编号', readonly=True),
+        'move_id': fields.many2one('stock.picking',u'单号', readonly=True),
         'product_id': fields.many2one('product.product',u'产品', readonly=True),
         'uom_id': fields.many2one('product.uom',u'单位', readonly=True),
         'product_qty': fields.float(u'数量', readonly=True),
@@ -36,14 +36,16 @@ class qdodoo_in_stock_report(osv.Model):
 
     def init(self, cr):
         tools.drop_view_if_exists(cr, 'qdodoo_in_stock_report')
+        location_model, location_ids = self.pool.get('ir.model.data').get_object_reference(cr, SUPERUSER_ID, 'stock', 'stock_location_suppliers')
+        location_model_cus, location_cus_ids = self.pool.get('ir.model.data').get_object_reference(cr, SUPERUSER_ID, 'stock', 'stock_location_customers')
         cr.execute("""
             create or replace view qdodoo_in_stock_report as (
                 select
                     sm.id as id,
                     sm.location_dest_id as location_id,
-                    sm.date as date,
-                    sm.partner_id as partner_id,
-                    pp.default_code as default_code,
+                    sp.date_done as date,
+                    sp.partner_id as partner_id,
+                    sp.id as move_id,
                     sm.product_id as product_id,
                     sm.product_uom as uom_id,
                     sm.product_uom_qty as product_qty,
@@ -51,11 +53,11 @@ class qdodoo_in_stock_report(osv.Model):
                     (sm.price_unit * sm.product_uom_qty) as amount
                 from stock_move sm
                     LEFT JOIN stock_picking sp ON sp.id = sm.picking_id
-                    LEFT JOIN product_product pp ON pp.id = sm.product_id
-                where sm.state='done'
+                    LEFT JOIN stock_location sl ON sm.location_id = sl.id
+                where sm.state='done' and (sl.usage='transit' or sl.id=%s) and sm.location_dest_id != %s
                 group by
-                    sm.id, sm.location_dest_id, sm.partner_id, pp.default_code, sm.product_id,
+                    sm.id, sm.location_dest_id, sm.product_id,sp.date_done,sp.id,sp.partner_id,
                     sm.price_unit, sm.product_uom_qty, sm.product_uom
-        )""")
+        )"""% (location_ids,location_cus_ids))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
