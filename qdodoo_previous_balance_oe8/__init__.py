604b48f88630ebe598898faef008764bf5237815
zhoubohui <zhoubohui@163.com>
2015-09-21 12:11:59 +0800

    1

---
+++ qdodoo_previous_balance_oe8/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+###########################################################################################
+#
+#    module name for OpenERP
+#    Copyright (C) 2015 qdodoo Technology CO.,LTD. (<http://www.qdodoo.com/>).
+#
+###########################################################################################
+
+  #引入模型
+import wizard
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
