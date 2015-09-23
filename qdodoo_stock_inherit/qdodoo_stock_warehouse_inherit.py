af4d8bfc3e4b1d878f1d363a69ff1ac12bcb6031
tfs <979581151@qq.com>
2015-09-23 22:13:50 +0800

    修改仓库权限模块

--- qdodoo_stock_inherit/qdodoo_stock_warehouse_inherit.py
+++ qdodoo_stock_inherit/qdodoo_stock_warehouse_inherit.py
@@ -6,7 +6,7 @@
 #
 ###########################################################################################
 
-from openerp import fields, models
+from openerp import fields, models, api
 import time
 from openerp.tools.translate import _
 from datetime import timedelta, datetime
@@ -15,13 +15,44 @@ from openerp import SUPERUSER_ID
 
 _logger = logging.getLogger(__name__)
 
+class qdodoo_res_user_inherit(models.Model):
+    """
+        在用户中增加仓库
+    """
+    _inherit = 'res.users'    # 继承
+
+    warehouse_id = fields.Many2many('stock.warehouse','user_warehouse_rel','user_id','warehouse_id',u'管理的仓库')
+
+    def create(self, cr, uid, value, context=None):
+        res_id = super(qdodoo_res_user_inherit, self).create(cr, uid, value, context=context)
+        obj = self.browse(cr, uid, res_id)
+        if value.get('warehouse_id'):
+            for line in obj.warehouse_id:
+                sql_select = "delete from warehouse_user_rel where user_id=%s"%obj.id
+                cr.execute(sql_select)
+                sql = """INSERT INTO warehouse_user_rel (user_id, warehouse_id) VALUES (%s, %s)"""%(obj.id, line.id)
+                cr.execute(sql)
+        return res_id
+
+    def write(self, cr, uid, ids, value, context=None):
+        super(qdodoo_res_user_inherit, self).write(cr, uid, ids, value, context=context)
+        obj = self.browse(cr, uid, ids[0])
+        if value.get('warehouse_id'):
+            for line in obj.warehouse_id:
+                sql_select = "delete from warehouse_user_rel where user_id=%s"%obj.id
+                cr.execute(sql_select)
+                sql = """INSERT INTO warehouse_user_rel (user_id, warehouse_id) VALUES (%s, %s)"""%(obj.id, line.id)
+                cr.execute(sql)
+        return True
+
+
 class qdodoo_stock_warehouse_inherit(models.Model):
     """
-        仓库所有者权限
+        在仓库中增加管理者
     """
     _inherit = 'stock.warehouse'    # 继承
 
-    owner_line = fields.Many2many('res.users','warehouse_id','user_id',u'所有者')
+    owner_line = fields.Many2many('res.users','warehouse_user_rel','warehouse_id','user_id',u'所有者')
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
