f65154e8a60d9662d97714187737c95cc9b0a0e7
Eric-China <1049495826@qq.com>
2015-10-27 15:44:34 +0800

    入库分析表增加库位字段及查询条件
    直接升级即可

--- qdodoo_stock_in_report/qdodoo_stock_in_report.py
+++ qdodoo_stock_in_report/qdodoo_stock_in_report.py
@@ -18,6 +18,7 @@ class qdodoo_stock_in_report(models.Model):
     product_id = fields.Many2one('product.product', string=u'产品')
     product_qty = fields.Float(digits=(16, 4), string=u'数量')
     product_amount = fields.Float(digits=(16, 4), string=u'金额')
+    location_id = fields.Many2one('stock.location', string=u'库位')
 
 
 class qdodoo_search_stock_in(models.Model):
@@ -25,23 +26,27 @@ class qdodoo_search_stock_in(models.Model):
 
     date_start = fields.Date(string=u'开始时间')
     date_end = fields.Date(string=u'结束时间')
+    location_id = fields.Many2one('stock.location', string=u'库位', required=True)
 
     @api.multi
     def action_done(self):
         purchase_obj = self.env['purchase.order']
         if self.date_start and not self.date_end:
             datetime_start = self.date_start + " 00:00:01"
-            purchase_ids = purchase_obj.search([('date_order', '>=', datetime_start), ('state', '=', 'done')])
+            purchase_ids = purchase_obj.search([('date_order', '>=', datetime_start), ('state', '=', 'done'),
+                                                ('location_id', '=', self.location_id.id)])
         elif not self.date_start and self.date_end:
             datetime_end = self.date_end + " 23:59:59"
-            purchase_ids = purchase_obj.search([('date_order', '<=', datetime_end), ('state', '=', 'done')])
+            purchase_ids = purchase_obj.search(
+                [('date_order', '<=', datetime_end), ('state', '=', 'done'), ('location_id', '=', self.location_id.id)])
         elif self.date_start and self.date_end:
             datetime_start = self.date_start + " 00:00:01"
             datetime_end = self.date_end + " 23:59:59"
             purchase_ids = purchase_obj.search(
-                [('date_order', '>=', datetime_start), ('date_order', '<=', datetime_end), ('state', '=', 'done')])
+                [('date_order', '>=', datetime_start), ('date_order', '<=', datetime_end), ('state', '=', 'done'),
+                 ('location_id', '=', self.location_id.id)])
         else:
-            purchase_ids = purchase_obj.search([('state', '=', 'done')])
+            purchase_ids = purchase_obj.search([('state', '=', 'done'), ('location_id', '=', self.location_id.id)])
 
         product_list = []
         product_dict = {}
@@ -65,15 +70,17 @@ class qdodoo_search_stock_in(models.Model):
                 'code': self.env['product.product'].browse(pro_l[0]).default_code,
                 'product_id': pro_l[0],
                 'product_qty': product_dict.get(pro_l, 0.0),
-                'product_amount': amount_dict.get(pro_l, 0.0)
+                'product_amount': amount_dict.get(pro_l, 0.0),
+                'location_id': self.location_id.id
 
             }
             create_obj = self.env['qdodoo.stock.in.report'].create(data)
             return_list.append(create_obj.id)
 
-        view_model, view_id = self.env['ir.model.data'].get_object_reference('qdodoo_stock_in_report', 'qdodoo_stock_in_report_tree')
+        view_model, view_id = self.env['ir.model.data'].get_object_reference('qdodoo_stock_in_report',
+                                                                             'qdodoo_stock_in_report_tree')
         return {
-            'name': _('工资核算'),
+            'name': _('入库分析表'),
             'view_type': 'form',
             "view_mode": 'tree',
             'res_model': 'qdodoo.stock.in.report',
