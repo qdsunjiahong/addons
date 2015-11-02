cb59b1bab252790152130c8b9e53a35f37015b0d
Until profile <2276298313@qq.com>
2015-11-02 21:06:53 +0800

    员工处显示日志
    员工处显示日志

--- qdodoo_hr_employee/models/qdodoo_hr_employee.py
+++ qdodoo_hr_employee/models/qdodoo_hr_employee.py
@@ -7,21 +7,26 @@
 #    Copyright (C) 2015 qdodoo Technology CO.,LTD. (<http://www.qdodoo.com/>).
 #
 ###########################################################################################
-
-from openerp.osv import osv, fields
+from  openerp import models,fields
 import datetime
 import urllib2
 from lxml import etree
 from openerp.tools.translate import _
 
 
-class qdodoo_hr_employee(osv.Model):
+class qdodoo_hr_employee(models.Model):
     """
         库存周转率
     """
     _inherit = 'hr.employee'
 
     # 字段定义
-    _columns = {
-        'work_data':fields.many2one("res.partner",'工作日志'),
-    }
+    work_data=fields.Integer(string='工作日志', compute='_edu_work_data')
+
+    def _edu_work_data(self):
+        '''
+            计算 学历记录数量
+        :return:
+        '''
+        Education_record = self.env['qdodoo.work.details']
+        self.work_data = Education_record.search_count([('employee','=',self.id)])
