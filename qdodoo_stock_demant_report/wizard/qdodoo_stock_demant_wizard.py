120ae29454ba768a3b21ee9e873abf6ef572667a
tfs <979581151@qq.com>
2015-11-18 10:34:45 +0800

    修改内部调拨差异数据异常问题

--- qdodoo_stock_demant_report/wizard/qdodoo_stock_demant_wizard.py
+++ qdodoo_stock_demant_report/wizard/qdodoo_stock_demant_wizard.py
@@ -59,6 +59,7 @@ class qdodoo_stock_demant_wizard(models.Model):
             sd_ids = self.env['qdodoo.stock.demand'].search(
                 [('company_id', 'in', company_ids_new),
                  ('date_planed', '>=', self.start_date), ('state', '=', 'done')])
+        result_list = []
         # 查询出来的需求转换单
         if sd_ids:
             sd_list = []  # 需求单列表
@@ -98,7 +99,6 @@ class qdodoo_stock_demant_wizard(models.Model):
                         in_num_dict[si_id.id] = in_num_dict.get(si_id.id, 0) + pick_id.product_uom_qty
                 location_s_id_dict[si_id.id] = location_s_id
                 location_d_id_dict[si_id.id] = location_d_id
-                result_list = []
                 data = {
                     'sd_id':si_id.id,
                     'location_id':location_s_id,
@@ -108,18 +108,18 @@ class qdodoo_stock_demant_wizard(models.Model):
                     'dif': out_num_dict.get(si_id.id, 0) - in_num_dict.get(si_id.id, 0),
                     'company_id': self.company_id.id
                 }
-                if out_num_dict.get(si_id.id, 0) - in_num_dict.get(si_id.id, 0) != 0:
+                if (out_num_dict.get(si_id.id, 0) - in_num_dict.get(si_id.id, 0)) != 0:
                     cre_obj = report_obj.create(data)
                     result_list.append(cre_obj.id)
-                view_mod, view_id = self.env['ir.model.data'].get_object_reference('qdodoo_stock_demant_report',
-                                                                                       'qdodoo_stock_demand_report_tree')
-                return {
-                    'name': _('内部调拨转移差'),
-                    'view_type': 'form',
-                    "view_mode": 'tree',
-                    'res_model': 'qdodoo.stock.demant.report',
-                    'type': 'ir.actions.act_window',
-                    'domain': [('id', 'in', result_list)],
-                    'views': [(view_id, 'tree')],
-                    'view_id': [view_id],
-                }
+        view_mod, view_id = self.env['ir.model.data'].get_object_reference('qdodoo_stock_demant_report',
+                                                                               'qdodoo_stock_demand_report_tree')
+        return {
+            'name': _('内部调拨转移差'),
+            'view_type': 'form',
+            "view_mode": 'tree',
+            'res_model': 'qdodoo.stock.demant.report',
+            'type': 'ir.actions.act_window',
+            'domain': [('id', 'in', result_list)],
+            'views': [(view_id, 'tree')],
+            'view_id': [view_id],
+        }
