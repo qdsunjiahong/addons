d7f7b7e7b758261ea57ff61dc4a72ac5686ff8ca
Eric-China <1049495826@qq.com>
2015-11-23 12:09:19 +0800

    中国会计模块

--- oecn_account_print/qdodoo_search_account_periodly.py
+++ oecn_account_print/qdodoo_search_account_periodly.py
@@ -22,10 +22,8 @@ class account_periodly_search(models.Model):
 
     @api.multi
     def btn_search(self):
-        print 11111
         periodly_ids = self.env['account.periodly'].search([])
         periodly_ids.unlink()
-        print 222222
         per_list = []
         per_ids = self.env['account.period'].search(
             [('date_start', '>=', self.start_p.date_start), ('date_stop', '<=', self.end_p.date_stop)])
@@ -102,47 +100,59 @@ class account_periodly_search(models.Model):
                 partner_line_dict[partner_key] = [(line[4], line[5], line[6], line[7], line[8])]
                 partner_key_list.append(partner_key)
         return_ids = []
+        # 获取账期开始时间
+        # 获取期初
+        sql3 = """
+            select
+                l.partner_id as partner_id,
+                l.account_id as account_id,
+                (l.debit-l.credit) as balance
+            from
+                account_move_line l
+                left join account_period p on (l.period_id=p.id)
+            where
+                p.date_start < '%s'
+            """ % (self.start_p.date_start)
+        self.env.cr.execute(sql3)
+        starting_balance_dict = {}  # 科目期初
+        start_partner_dict = {}  # 客户期初
+        res_balance = self.env.cr.fetchall()
+        if res_balance:
+            for balance_l in res_balance:
+                starting_balance_dict[balance_l[1]] = starting_balance_dict.get(balance_l[1], 0) + balance_l[2]
+                start_partner_dict[balance_l[:2]] = start_partner_dict.get(balance_l[:2], 0) + balance_l[2]
         for key in partner_key_list:
             if key[1:] not in for_list:
-                # 获取账期开始时间
-                # 获取期初
-                sql3 = """
-                select
-                    sum(l.debit-l.credit) as balance
-                from
-                    account_move_line l
-                    left join account_period p on (l.period_id=p.id)
-                where
-                    p.date_start < '%s' and l.account_id = %s and l.company_id = %s
-                """ % (self.start_p.date_start, key[2], key[3])
-                self.env.cr.execute(sql3)
-                res = self.env.cr.fetchall()
-                starting_balance = 0.0
-                if res[0][0]:
-                    starting_balance = res[0][0]
-                ending_balance = starting_balance + debit_dict.get(key[1:], 0) - credit_dict.get(key[1:], 0)
+                ending_balance = starting_balance_dict.get(key[2], 0) + debit_dict.get(key[1:], 0) - credit_dict.get(
+                    key[1:], 0)
                 balance = debit_dict.get(key[1:], 0) - credit_dict.get(key[1:], 0)
                 sql_1 = """
                 insert into account_periodly (period_id,starting_balance,ending_balance,account_id,company_id,debit,credit,balance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) returning id
-                """ % (key[1], starting_balance, ending_balance, key[2], key[3], debit_dict.get(key[1:], 0),
+                """ % (key[1], starting_balance_dict.get(key[2], 0), ending_balance, key[2], key[3],
+                       debit_dict.get(key[1:], 0),
                        credit_dict.get(key[1:], 0), balance)
                 self.env.cr.execute(sql_1)
                 sql_result1 = self.env.cr.fetchall()
                 for_dict[key[1:]] = sql_result1[0][0]
                 for_list.append(key[1:])
                 return_ids.append(sql_result1[0][0])
+            starting_balance_partner = start_partner_dict.get((key[0], key[2]), 0)
+            ending_balance_partner = start_partner_dict.get((key[0], key[2]), 0) + partner_dict_debit.get(key,
+                                                                                                          0) - partner_dict_credit.get(
+                key, 0)
+
             if key[0]:
                 sql_2 = """
-                insert into qdodoo_account_partner_report (account_periodly_id,partner_id,period_id,account_id,company_id,debit,credit) VALUES (%s,%s,%s,%s,%s,%s,%s) returning id
+                insert into qdodoo_account_partner_report (account_periodly_id,partner_id,period_id,account_id,company_id,debit,credit,start_balance,end_balance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) returning id
                 """ % (
                     for_dict.get(key[1:]), key[0], key[1], key[2], key[3], partner_dict_debit.get(key, 0),
-                    partner_dict_credit.get(key, 0))
+                    partner_dict_credit.get(key, 0), starting_balance_partner, ending_balance_partner)
             else:
                 sql_2 = """
-                insert into qdodoo_account_partner_report (account_periodly_id,period_id,account_id,company_id,debit,credit) VALUES (%s,%s,%s,%s,%s,%s) returning id
+                insert into qdodoo_account_partner_report (account_periodly_id,period_id,account_id,company_id,debit,credit,start_balance,end_balance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) returning id
                 """ % (
                     for_dict.get(key[1:]), key[1], key[2], key[3], partner_dict_debit.get(key, 0),
-                    partner_dict_credit.get(key, 0))
+                    partner_dict_credit.get(key, 0), starting_balance_partner, ending_balance_partner)
             self.env.cr.execute(sql_2)
             sql_result2 = self.env.cr.fetchall()
             if partner_line_dict.get(key, False):
@@ -177,8 +187,10 @@ class qdodoo_account_partner_report(models.Model):
     partner_id = fields.Many2one('res.partner', string=u'业务伙伴', readonly=True)
     period_id = fields.Many2one('account.period', string=u'期间', readonly=True)
     company_id = fields.Many2one('res.company', string=u'公司', readonly=True)
+    start_balance = fields.Float(string=u'起初余额', readonly=True)
     debit = fields.Float(string=u'借方', readonly=True)
     credit = fields.Float(string=u'贷方', readonly=True)
+    end_balance = fields.Float(string=u'期末', readonly=True)
     account_id = fields.Many2one('account.account', string=u'科目', readonly=True)
     line_ids = fields.One2many('qdodoo.account.partner.line', 'report_id', string=u'明细')
 
@@ -194,4 +206,4 @@ class qdodoo_account_partner_line(models.Model):
     credit = fields.Float(string=u'贷方', readonly=True)
     account_id = fields.Many2one('account.account', string=u'科目', readonly=True)
     move_name = fields.Char(string=u'凭证号', readonly=True)
-    line_name = fields.Char(string=u'说明', readonly=True)
+    line_name = fields.Char(string=u'名称', readonly=True)
