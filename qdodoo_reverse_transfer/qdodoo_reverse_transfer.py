b93d3649f7afd0f65733fb062ba94da3be51c17d
Eric-China <1049495826@qq.com>
2015-11-29 22:39:16 +0800

    退货取采购/销售价格

--- qdodoo_reverse_transfer/qdodoo_reverse_transfer.py
+++ qdodoo_reverse_transfer/qdodoo_reverse_transfer.py
@@ -22,29 +22,38 @@ class qdodoo_reverse_transfer(models.Model):
         record_id = context and context.get('active_id', False) or False
         if not record_id:
             raise except_orm(_(u'警告'), _(u'移动单与采购单关联异常'))
-        query = """
-        SELECT
-          po.id
-        FROM stock_picking p,
-             stock_move m,
-             purchase_order_line pol,
-             purchase_order po
-        WHERE p.id = %s and po.id = pol.order_id and pol.id = m.purchase_line_id and m.picking_id = p.id
-        GROUP BY picking_id, po.id
-        """ % record_id
-        self.env.cr.execute(query)
-        res = self.env.cr.fetchall()
-        if res and len(res) == 1:
-            purchase_id = self.env['purchase.order'].browse(res[0][0])
-            if purchase_id.order_line:
-                for order_id in purchase_id.order_line:
-                    product_price_list[order_id.product_id.id] = order_id.price_unit
-        else:
-            raise except_orm(_(u'警告'), _(u'反向转移失败!'))
-        new_picking_id, pick_type_id = super(qdodoo_reverse_transfer,self)._create_returns()
-        picking_id = self.env['stock.picking'].browse(new_picking_id)
-        if picking_id.move_lines:
-            for move_line in picking_id.move_lines:
+        sale_obj = self.env['sale.order']
+        picking_obj = self.env['stock.picking']
+        code = picking_obj.browse(record_id).picking_type_id.code
+        if code == "outgoing":
+            picking_id = picking_obj.browse(record_id)
+            sale_id = sale_obj.search([('procurement_group_id', '=', picking_id.group_id.id)])
+            if not sale_id or len(sale_id) > 1:
+                raise except_orm(_(u'警告'), _(u'销售单与移库蛋关联异常'))
+            for move_l in sale_id.order_line:
+                product_price_list[move_l.product_id.id] = move_l.price_unit
+        elif code == 'incoming':
+            query = """
+                SELECT
+                  po.id
+                FROM stock_picking p,
+                     stock_move m,
+                     purchase_order_line pol,
+                     purchase_order po
+                WHERE p.id = %s and po.id = pol.order_id and pol.id = m.purchase_line_id and m.picking_id = p.id
+                GROUP BY picking_id, po.id
+            """ % record_id
+            self.env.cr.execute(query)
+            res = self.env.cr.fetchall()
+            if res and len(res) == 1:
+                purchase_id = self.env['purchase.order'].browse(res[0][0])
+                if purchase_id.order_line:
+                    for order_id in purchase_id.order_line:
+                        product_price_list[order_id.product_id.id] = order_id.price_unit
+        new_picking_id, pick_type_id = super(qdodoo_reverse_transfer, self)._create_returns()
+        picking_id2 = picking_obj.browse(new_picking_id)
+        if picking_id2.move_lines:
+            for move_line in picking_id2.move_lines:
                 product_id = move_line.product_id.id
                 if product_id in product_price_list:
                     move_line.write({'price_unit': product_price_list.get(product_id)})
