eabece0422a26b1866fb9106ccc9b01f51a375de
Eric-China <1049495826@qq.com>
2015-12-08 17:21:33 +0800

    生产订单增加强制会计区间

--- qdodoo_mrp_materials/qdodoo_mrp_materials.py
+++ qdodoo_mrp_materials/qdodoo_mrp_materials.py
@@ -15,24 +15,28 @@ from openerp import SUPERUSER_ID
 
 _logger = logging.getLogger(__name__)
 
+
 class qdodoo_mrp_materials(models.Model):
     """
         生产订单增加强制会计区间字段
     """
     _inherit = 'mrp.production'
 
-    qdodoo_period = fields.Many2one('account.period',u'强制会计区间')
+    qdodoo_period = fields.Many2one('account.period', u'强制会计区间')
+
 
 class qdodoo_stock_move_tfs(models.Model):
     _inherit = 'stock.move'
 
-    qdodoo_period = fields.Many2one('account.period',u'强制会计区间')
+    qdodoo_period = fields.Many2one('account.period', u'强制会计区间')
+
 
 class qdodoo_stock_quant_tfs(models.Model):
     _inherit = 'stock.quant'
 
-    def _create_account_move_line(self, cr, uid, quants, move, credit_account_id, debit_account_id, journal_id, context=None):
-        #group quants by cost
+    def _create_account_move_line(self, cr, uid, quants, move, credit_account_id, debit_account_id, journal_id,
+                                  context=None):
+        # group quants by cost
         quant_cost_qty = {}
         for quant in quants:
             if quant_cost_qty.get(quant.cost):
@@ -42,10 +46,12 @@ class qdodoo_stock_quant_tfs(models.Model):
         move_obj = self.pool.get('account.move')
         production_obj = move.production_id or move.raw_material_production_id
         for cost, qty in quant_cost_qty.items():
-            move_lines = self._prepare_account_move_line(cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=context)
-            period_id =production_obj.qdodoo_period.id if production_obj and production_obj.qdodoo_period else context.get('force_period', self.pool.get('account.period').find(cr, uid, move.date, context=context)[0])
+            move_lines = self._prepare_account_move_line(cr, uid, move, qty, cost, credit_account_id, debit_account_id,
+                                                         context=context)
+            period_id = production_obj.qdodoo_period.id if production_obj and production_obj.qdodoo_period else context.get(
+                'force_period', self.pool.get('account.period').find(cr, uid, move.date, context=context)[0])
             move_obj.create(cr, uid, {'journal_id': journal_id,
                                       'line_id': move_lines,
-                                      'period_id': period_id,
+                                      'period_id': move.period_id.id or period_id,
                                       'date': move.date,
                                       'ref': move.picking_id.name}, context=context)
