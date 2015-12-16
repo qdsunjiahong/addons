12015107cde6757a90e47040fb3113b3e8fa015d
Eric-China <1049495826@qq.com>
2015-12-16 16:17:08 +0800

    产品盘点库位

---
+++ qdodoo_create_product/qdodoo_create_product.py
@@ -0,0 +1,20 @@
+# encoding:utf-8
+
+from openerp import models, api, _
+from openerp.exceptions import except_orm
+
+
+class qdodoo_product_template(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def create(self, vals):
+        print vals,"opopopop"
+        res = super(qdodoo_product_template, self).create(vals)
+        company_id = vals.get('company_id', False)
+        if company_id:
+
+            location_id = self.env['stock.location'].search(
+                [('company_id', '=', company_id), ('usage', '=', 'inventory')])
+            res.write({'property_stock_inventory': location_id})
+        return res
