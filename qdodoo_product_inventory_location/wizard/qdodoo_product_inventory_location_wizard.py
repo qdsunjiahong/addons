3760aeca8d4e9bdfb919f1fe38385c919e5da8c8
Eric-China <1049495826@qq.com>
2016-01-13 14:59:07 +0800

    产品盘点库位调整

--- qdodoo_product_inventory_location/wizard/qdodoo_product_inventory_location_wizard.py
+++ qdodoo_product_inventory_location/wizard/qdodoo_product_inventory_location_wizard.py
@@ -16,25 +16,21 @@ class qdodoo_product_inventory_location(models.Model):
 
     @api.multi
     def action_done(self):
+        company_ids = self.env['res.users'].browse(self.env.uid).company_id
+        company_id = company_ids.id
         location_obj = self.env['stock.location']
-        location_ids = location_obj.search([('usage', '=', 'inventory')])
-        company_location_dict = {}  # {company_id:location_id}
-        for location_id in location_ids:
-            company_id = location_id.company_id.id
-            location_id = location_id.id
-            if company_id in company_location_dict:
-                raise except_orm(_(u'警告'), _(u'公司"%s"存在多个盘点类型库位') % location_id.company_id.name)
-            else:
-                company_location_dict[company_id] = location_id
+        location_ids = location_obj.search([('usage', '=', 'inventory'), ('company_id', '=', company_id)])
+        if not location_ids:
+            raise except_orm(_(u'警告'), _(u'公司"%s"未创建盘点类型库位') % company_ids.name)
+        elif len(location_ids) > 1:
+            raise except_orm(_(u'警告'), _(u'公司"%s"存在多个盘点类型库位') % company_ids.name)
         product_obj = self.env['product.template']
-        product_ids = product_obj.search([('type', '!=', 'service')])
+        product_ids = product_obj.search([('type', '!=', 'service'), ('company_id', '=', company_id)])
 
         result_list = []
         for product_id in product_ids:
-            location_id1 = company_location_dict.get(product_id.company_id.id, '')
+            location_id1 = location_ids.id
             location_id2 = product_id.property_stock_inventory.id or ''
-            if location_id1 == '':
-                raise except_orm(_(u'警告'), _(u'公司"%s"未创建盘点库位') % product_id.company_id.name)
             if location_id1 != location_id2:
                 product_id.write({'property_stock_inventory': location_id1})
                 result_list.append(product_id.id)
