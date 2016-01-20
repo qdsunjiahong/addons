b5ba051fdb30a2a111b0642ed3d54a422c490920
tfs <979581151@qq.com>
2016-01-20 11:05:00 +0800

    微信支付模块

---
+++ wxsite/static/js/index.js
@@ -0,0 +1,50 @@
+//重设顶部菜单内容宽度
+var catsLength = $('.top-cats ul:first li').length*4;
+$('.top-cats ul:first').css('width', catsLength+'em');
+
+//顶部菜单拖拽
+var dragX = 0;
+$('.top-cats ul:first').drag({
+    process: function(e){
+        var pageX = e.pageX;
+        
+        if(dragX == 0)dragX = pageX;
+        
+        var moveX = parseInt(pageX - dragX);
+        if(moveX > 0)moveX = 0;
+        
+        
+        $('.top-cats ul:first').css({
+            'left': moveX+'px',
+            'top': '0'
+        });
+    },
+    end: function(e){
+//        var pageX = e.pageX;
+//        
+//        if(dragX != pageX)dragX = pageX;
+    }
+});
+
+
+//顶部定时滚动
+setInterval(function(){
+    var currIndex = 0;
+
+    $('.index-scrolls a').each(function(){
+        if($(this).find('img').hasClass('curr-img')){
+            currIndex = $(this).index();
+        }
+    });
+
+    var scrollNum = $('.index-scrolls img').length,
+        nextIndex = currIndex + 1>scrollNum-1?0:currIndex+1;
+
+    $('.index-scrolls img.curr-img').removeClass('curr-img');
+    $('.index-scrolls a:eq('+nextIndex+')').find('img').addClass('curr-img');
+
+    $('.scroll-marks li.curr-mark').removeClass('curr-mark');
+    $('.scroll-marks li:eq('+nextIndex+')').addClass('curr-mark');
+
+
+}, 3000);
