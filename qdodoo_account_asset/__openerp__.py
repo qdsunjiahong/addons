3e1e5912f970c34c7abe5fca152749dbc8ae790e
tfs <979581151@qq.com>
2016-03-11 14:15:46 +0800

    修改固定资产模块

--- qdodoo_account_asset/__openerp__.py
+++ qdodoo_account_asset/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     'name': "中国固定资产修改",
-    'version': '1.1',
+    'version': '2.0',
     'author': 'Qdodoo suifeng',
     'category': 'Account',
     'sequence': 21,
@@ -17,6 +17,9 @@
 修改内容：\n
 1：按照中国习惯，生成折旧数据是从采购日期的下月一号开始 \n
 2：折旧生成的会计凭证日期为折旧月份的最后一天 \n
+3：折旧生成的会计分录按照凭证簿合并会计分录
+
+有问题可以邮件 qdodoo@qdodoo.com
     """,
     'images': [
     ],
