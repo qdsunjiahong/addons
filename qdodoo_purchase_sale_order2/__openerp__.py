827823014fbee24aebf764e26b39bb32d0582753
tfs <979581151@qq.com>
2016-05-03 09:31:01 +0800

    修改报价单客户不能带出辅助核算项问题

--- qdodoo_purchase_sale_order2/__openerp__.py
+++ qdodoo_purchase_sale_order2/__openerp__.py
@@ -43,7 +43,7 @@
     'installable': True,  # 是否可安装
     'auto_install': False,  # 是否自动安装
     'description': """
-    采购转销售增加辅助核算项
+    采购转销售补充模块
     """,
 }
 
--- qdodoo_purchase_sale_order2/qodooo_purchase_sale_order.py
+++ qdodoo_purchase_sale_order2/qodooo_purchase_sale_order.py
@@ -5,7 +5,7 @@
 #    Copyright (C) 2015 qdodoo Technology CO.,LTD. (<http://www.qdodoo.com/>).
 #
 ###########################################################################################
-from openerp import models, fields,_
+from openerp import models, fields,_, api
 from openerp.osv.osv import except_osv
 
 class qdodoo_sale_order_inherit(models.Model):
@@ -14,9 +14,17 @@ class qdodoo_sale_order_inherit(models.Model):
     """
     _inherit = 'sale.order'  # 继承
 
+    # location_id_note = fields.Char(u'目的仓库备注')
     project_id = fields.Many2one('account.analytic.account', 'Contract / Analytic', readonly=True,
                                  states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
                                  required=True, help="The analytic account related to a sales order.")
+
+    def onchange_partner_id(self, cr, uid, ids, part, context=None):
+        res = super(qdodoo_sale_order_inherit, self).onchange_partner_id(cr, uid, ids, part, context=context)
+        part = self.pool.get('res.partner').browse(cr, uid, part, context=context)
+        if part and part.analytic_account_id:
+            res['value'].update({'project_id':part.analytic_account_id.id})
+        return res
 
 
 class qdodoo_res_partner_inherit(models.Model):
